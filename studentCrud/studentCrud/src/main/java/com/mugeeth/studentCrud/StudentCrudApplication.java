9a93c1e82c47e8aad083dcc6a5e1a1ab760b03b3
mugeeth02 <122195278+mugeeth02@users.noreply.github.com>
2024-07-31 17:33:24 +0530

    manoj

--- studentCrud/studentCrud/src/main/java/com/mugeeth/studentCrud/StudentCrudApplication.java
+++ studentCrud/studentCrud/src/main/java/com/mugeeth/studentCrud/StudentCrudApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 
-
+//manoj
 @SpringBootApplication
 public class StudentCrudApplication {
 
